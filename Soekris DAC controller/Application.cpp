da58029ab7629d2b37f00e1548c7631698edaacb
joongsupchoi-star <joongsup.choi@gmail.com>
2026-08-17 10:22:37 +0900

    Delete Soekris DAC controller directory

--- Soekris DAC controller/Application.cpp
+++
@@ -1,9 +0,0 @@
-#define ARDUINO 10800
-#define __AVR_ATmega328P__ // Arduino Nano
-
-#include <VirtualBoard.h>
-
-// Include additional Arduino libraries *.cpp files here
-//#include "../../../libraries/RfControl/src/RfControl.cpp"
-
-#include "Soekris DAC controller/Sketch.h"
